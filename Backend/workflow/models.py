74360ce3ffecc69aedfd85df6f0ecbc8e95947c6
Guilherme Ramalho <158581301+GuilhermeRamalho10@users.noreply.github.com>
2024-02-20 09:17:15 +0100

    ajustando caminhos de fotos

--- Backend/workflow/models.py
+++ Backend/workflow/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Photo(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField(upload_to='photos/')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
